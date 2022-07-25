8e06292b6383c4fc52b15ccd79c356e34219af90
fab-ryan <royalfabrice1234@gmail.com>
2022-07-25 12:24:45 +0200

    finishup of delete and update

--- src/mutation/categoryMutation.ts
+++ src/mutation/categoryMutation.ts
@@ -23,6 +23,60 @@ export const CategoryMutation = extendType({
                 })
                 return newCategory;
             }
+
+        })
+        t.field("updateCategory", {
+            type: "Category",
+            args: {
+                id: nonNull(stringArg()),
+                categoryName: nonNull(stringArg()),
+                categoryDescription: nonNull(stringArg()),
+            },
+            async resolve(parent, args, context, info) {
+                const { categoryName, categoryDescription, id } = args;
+                const userId = context.userId;
+
+                if (!userId) {
+                    throw new Error("Not Authorized");
+                }
+                const updatedFound = await context.prisma.category.findUnique({
+                    where: { id }
+                })
+                if (!updatedFound) {
+                    throw new Error("Category not found")
+                }
+                const updatedCategory = await context.prisma.category.update({
+                    where: { id },
+                    data: {
+                        categoryName,
+                        categoryDescription,
+                        createdBy: { connect: { id: userId } }
+                    }
+                })
+                return updatedCategory;
+            }
+        })
+        t.field("deleteCategory", {
+            type: "Category",
+            args: {
+                id: nonNull(stringArg()),
+            },
+            async resolve(parent, args, context, info) {
+                const { id } = args;
+
+                const userId = context.userId;
+                if (!userId) {
+                    throw new Error("Not Authorized");
+                }
+                const categoryFound = await context.prisma.category.findUnique({
+                    where: { id }
+                })
+                if (!categoryFound) { throw new Error("Category not found") }
+                const deletedCategory = await context.prisma.category.delete({
+                    where: { id },
+                })
+                return deletedCategory;
+            }
         })
     }
 })

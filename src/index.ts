83979a530cd076243e5ad53ee3ae3f9c7b4028ad
fab-ryan <royalfabrice1234@gmail.com>
2022-07-28 12:45:56 +0200

    caching

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ import { ApolloServer } from "apollo-server";
 import * as schema from "./schema/index";
 import { context } from "./schema/context"
 import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
-
+import { InMemoryLRUCache } from '@apollo/utils.keyvaluecache';
 
 export const server = new ApolloServer({
     schema: schema.schema,
     context,
     plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    cache: new InMemoryLRUCache({
+        // ~100MiB
+        maxSize: Math.pow(2, 20) * 100,
+        // 5 minutes (in milliseconds)
+        ttl: 300_000,
+    }),
+
 });
